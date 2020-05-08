2a7e03b343d8815bafa3f198b2389912c49f51f3
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2020-05-08 01:22:56 -0400

    Basic functionality

--- src/RunLengthCodec.jl
+++ src/RunLengthCodec.jl
@@ -1,5 +1,72 @@
 module RunLengthCodec
 
-# Write your package code here.
+import Base.Iterators: cycle
+"""
+Take some iterable of symbols and produce a vector `[(symbol, count), ...]`
+"""
+function encode(input)
+    output = Tuple{eltype(input), Int}[]
+    for e in input
+        if length(output) == 0
+            push!(output, (e, 1))
+            continue
+        end
+
+        if isequal(output[end][1], e)
+            # TODO use a Lens package (setfield?)
+            output[end] = (e, output[end][2] + 1)
+        else
+            push!(output, (e, 1))
+        end
+    end
+    return output
+end
+
+
+function decode(input)
+  T#=::Type{Tuple}=# = eltype(input)
+  (output_eltype, count_type) = Tuple(T.parameters)
+  output_n = sum(last.(input))
+  output = Array{output_eltype}(undef, output_n)
+
+  decode!(output, input)
+  return output
+end
+
+
+function decode!(output, input)
+  i = 1
+  for (v, c) in input
+    j = i + c
+    output[i:(j-1)] .= Ref(v)
+    i = j
+  end
+  return output
+end
+
+
+
+"""
+A of `true`/`false` values, when RLE'd, alternates between true/false.
+So it is enough to just encode the counts, provided we always start on `false`,
+which is guaranteed by adding a count of `0` when necessary.
+"""
+function bool_normalize(rle)
+    # ensure we start on `false`
+    prefix = if first(first(rle))
+        [0]
+    else
+        Int[]
+    end
+    return vcat(prefix, last.(rle))
+end
+
+inv_bool_normalize(input) = zip(cycle((false, true)), input)
+
+bool_encode(input) = bool_normalize(encode(input))
+
+function bool_decode(input)
+  decode(inv_bool_normalize(input))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
-using RunLengthCodec
+using RunLengthCodec: encode, decode, bool_encode, bool_decode
 using Test
 
 @testset "RunLengthCodec.jl" begin
-    # Write your tests here.
+  @test 0 == length(encode([]))
+  @test [(0, 2), (1, 1), (0, 3)] == encode([0,0,1,0,0,0])
+end
+
+@testset "all bytes" begin
+  for x = collect(Iterators.product(ntuple(_->(false, true), 8)...))
+    @test collect(x) == bool_decode(bool_encode(x))
+  end
 end
